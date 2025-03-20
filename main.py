2fad63a80062507e8c934e3e3f196fabe56ef50c
a797405 <arek90urban@gmail.com>
2025-03-20 15:47:20 +0100

    version 1.0

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from random import choice
+from tabnanny import check
+
 
 class Player():
 
@@ -29,7 +31,10 @@ class UserPlayer(Player):
 
     def choose(self):
 
-        self.weapon = input("choose Rock or Paper or Scissors [r,p,s]: ")[0].lower()
+        try:
+            self.weapon = input(f"{self.name}, please choose Rock or Paper or Scissors [r,p,s]: ")[0].lower()
+        except IndexError:
+            self.weapon = input("Please choose Rock or Paper or Scissors [r,p,s]: ")[0].lower()
 
 class Computer(Player):
     def __init__(self, name):
@@ -49,12 +54,34 @@ class Game():
         self.player_1 = player_1
         self.player_2 = player_2
         self.round_winner = None
+        self.winner = None
 
     def next_round(self):
 
+        self.round += 1
+        print(f"round {self.round}")
         self.player_1.choose()
         self.player_2.choose()
         self.check_result()
+
+    def check_final_result(self):
+
+        if self.player_1.score > self.player_2.score:
+            self.winner = self.player_1.name
+        elif self.player_2.score > self.player_1.score:
+            self.winner = self.player_2.name
+        else:
+            self.winner = None
+
+    def play(self):
+
+        while self.round < self.round_limit:
+            self.next_round()
+            print(self.__str__())
+
+        self.check_final_result()
+        print(f"\nCongratulations!\nThe winner is {self.winner}!")
+
 
     def check_result(self):
 
@@ -72,23 +99,19 @@ class Game():
             self.round_winner = None
 
     def __str__(self):
-        return (f"round {self.round}\n{self.player_1.name}: {self.player_1.weapon}\n"
+        return (f"{self.player_1.name}: {self.player_1.weapon}\n"
                 f"{self.player_2.name}: {self.player_2.weapon}\n"
                 f"The winner of this round is {self.round_winner}\n"
-                f"Result: {self.player_1.name}: {self.player_1.score} - {self.player_2.name}: {self.player_2.score}\n")
+                f"Result: {self.player_1.name} {self.player_1.score} - {self.player_2.score} {self.player_2.name}\n")
 
 
 
 if __name__ == "__main__":
 
-    a = UserPlayer('Arek')
-
-
+    player = UserPlayer('Lewis')
     ai = Computer("AI")
-
-    game = Game(round_limit=1, player_1=a, player_2=ai)
-    game.next_round()
-    print(game)
+    game = Game(round_limit=3, player_1=player, player_2=ai)
+    game.play()
 
 
 
